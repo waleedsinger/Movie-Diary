029ba3c2ad9f778c671a271b3853eb290a7e0bfb
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-13 17:10:14 +0200

    adapted main.js, included interactivity of favorite icon, fixed errors

--- main.js
+++ main.js
@@ -82,14 +82,19 @@ function createMovieCard(movieObject) {
   const cardFavBtn = document.createElement("button");
   cardFavBtn.className =
     "absolute fill-current top-2 right-0.5 text-yellow-400 p-6 hover:text-yellow-500";
-  cardFavBtn.style = "background-image: url('./resources/icons8-stern-48.png')"
 
-//   cardFavBtn.textContent = `<svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-// <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-// </svg>`;
+    if (!favoritesArray.find((fav) => fav.id === movieObject.id)) {
+        cardFavBtn.style = "background-image: url('./resources/icons8-stern-48.png')";
+    }
+    else cardFavBtn.style = "background-image: url('./resources/icons8-stern-48-full.png')";
+
+    
+  let favToggle = false;
+
   cardFavBtn.addEventListener("click", (e) => {
     addToFavorites(movieObject);
     cardFavBtn.style = "background-image: url('./resources/icons8-stern-48-full.png')"
+    favToggle = true;
   });
 
   cardFavBtn.addEventListener("mouseover", (e) => {
@@ -97,6 +102,7 @@ function createMovieCard(movieObject) {
   });
 
   cardFavBtn.addEventListener("mouseout", (e) => {
+    if (!favToggle)
     cardFavBtn.style = "background-image: url('./resources/icons8-stern-48.png')"
   });
 
